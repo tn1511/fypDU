5f6aa2e8cd34aade07753b1fdc734bfb3879e292
tn1511 <tommy.a.n@outlook.com>
2023-02-28 13:54:39 +0000

    register authentication

--- app/src/main/java/com/example/fypdu/RegisterUser.java
+++ app/src/main/java/com/example/fypdu/RegisterUser.java
@@ -2,13 +2,97 @@ package com.example.fypdu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tagline;
+    private EditText editTextName, editTextAge, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+    private Button registerBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        tagline = (TextView) findViewById(R.id.tagline);
+        tagline.setOnClickListener(this);
+
+        registerBtn = (Button) findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(this);
+
+        editTextName = (EditText) findViewById(R.id.name);
+        editTextAge = (EditText) findViewById(R.id.age);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tagline:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerBtn:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+
+        if (name.isEmpty()){
+            editTextName.setError("Full Name is required");
+            editTextName.requestFocus();
+            return;
+        }
+        if (age.isEmpty()) {
+            editTextAge.setError("Age is required");
+            editTextAge.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Valid email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPassword.setError("Password longer than 5 characters is required");
+            editTextPassword.requestFocus();
+            return;
+
+        }
+
     }
 }
