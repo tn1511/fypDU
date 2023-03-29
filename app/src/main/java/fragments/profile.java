eb3974e84ce855f1c2f2de77694c484c97528732
tn1511 <tommy.a.n@outlook.com>
2023-03-29 14:29:24 +0100

    added profile page with a logout button

--- app/src/main/java/fragments/profile.java
+++ app/src/main/java/fragments/profile.java
@@ -1,7 +1,9 @@
 package fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.Gravity;
@@ -9,11 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fypdu.MainActivity;
 import com.example.fypdu.R;
+import com.example.fypdu.user;
+import com.example.fypdu.userProfile;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +46,8 @@ public class profile extends Fragment {
     private String userID;
 
     private Button logout;
+
+
 
     public profile() {
         // Required empty public constructor
@@ -75,7 +88,60 @@ public class profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        logout = (Button) v.findViewById(R.id.logoutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(view.getContext(), MainActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        userID = user.getUid();
+
+        final TextView introMsgTextView = (TextView) v.findViewById(R.id.introMsg);
+        final TextView fNameTextView = (TextView) v.findViewById(R.id.fName);
+        final TextView emailTextView = (TextView) v.findViewById(R.id.email);
+        final TextView ageTextView = (TextView) v.findViewById(R.id.age);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                com.example.fypdu.user userProfile = snapshot.getValue(user.class);
+                if (userProfile != null){
+                    String fname = userProfile.name;
+                    String email = userProfile.email;
+                    String age = userProfile.age;
+
+                    introMsgTextView.setText("welcome " + fname);
+                    fNameTextView.setText(fname);
+                    ageTextView.setText(age);
+                    emailTextView.setText(email);
+
+
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+        return v;
 
 
 
